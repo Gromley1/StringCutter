b1cd6a2bbc195c5743dfd5f97baec2b3c3770b74
gromley111 <gromley111@gmail.com>
2019-04-25 21:17:56 -0700

    adding program

--- StringCutter/Program.cs
+++ StringCutter/Program.cs
@@ -6,7 +6,30 @@ namespace StringCutter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool endApp = false;
+            while (!endApp)
+            {
+                Console.WriteLine("Please enter a word");
+                string str = Console.ReadLine();
+                WriteNewString(str);
+
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+            }
+        }
+
+        static void WriteNewString(string str)
+        {
+            string newStr = string.Empty;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((i + 1) % 3 == 0)
+                {
+                    newStr += str.Substring(i, 1);
+                }
+            }
+            Console.WriteLine(newStr);
+
         }
     }
 }
